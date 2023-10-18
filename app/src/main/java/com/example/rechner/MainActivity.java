ca7ff5fa1610656bc2705bc9180df33cbaa9bd00
mkurz-TGM <mkurz@student.tgm.ac.at>
2023-10-18 11:27:32 +0200

    shared preferences work

--- app/src/main/java/com/example/rechner/MainActivity.java
+++ app/src/main/java/com/example/rechner/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.rechner;
 
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,17 +15,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     Button button1;
+    Button buttonMS;
+    Button buttonMR;
+    EditText input1;
+    EditText input2;
+    RadioGroup group;
+    public static final String MY_PREFS = "MyPrefsFile";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         button1 = (Button) findViewById(R.id.button1);
-        EditText input1 = findViewById(R.id.input1);
-        EditText input2 = findViewById(R.id.input2);
-        RadioGroup group = findViewById(R.id.radioGroup);
+        buttonMS = (Button) findViewById(R.id.buttonMS);
+        buttonMR = findViewById(R.id.buttonMR);
 
-        Log.i("a", String.valueOf(button1));
+        input1 = findViewById(R.id.input1);
+        input2 = findViewById(R.id.input2);
+        group = findViewById(R.id.radioGroup);
+
+        buttonMS.setOnClickListener(view ->{
+            SharedPreferences.Editor editor = getSharedPreferences(MY_PREFS, MODE_PRIVATE).edit();
+            editor.putString("val1", input1.getText().toString());
+            editor.putString("val2", input2.getText().toString());
+            editor.apply();
+        });
+
+        buttonMR.setOnClickListener(view ->{
+            SharedPreferences prefs = getSharedPreferences(MY_PREFS, MODE_PRIVATE);
+            input1.setText(prefs.getString("val1", "-1"));
+            input2.setText(prefs.getString("val2", "-1"));
+
+        });
 
         button1.setOnClickListener(view -> {
             int val1 = Integer.parseInt(input1.getText().toString());
@@ -62,4 +84,6 @@ public class MainActivity extends AppCompatActivity {
         int color = Color.rgb(10,200,10);
         button1.setBackgroundColor(color);
     }
+
+
 }
