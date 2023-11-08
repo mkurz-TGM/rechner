328ab83810dc4f156237287b6afdaf1d2c79a087
mkurz-TGM <mkurz@student.tgm.ac.at>
2023-11-08 14:46:58 +0100

    update

--- app/src/main/java/com/example/rechner/MainActivity.java
+++ app/src/main/java/com/example/rechner/MainActivity.java
@@ -10,8 +10,6 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
